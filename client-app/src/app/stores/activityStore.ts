170b3eabc7b7da9fe1c5911344da3ae4765c1e97
Hoang Nguyen <hoangnguyen.vn208@gmail.com>
2019-10-02 21:42:51 +0300

    wip error handling

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -124,13 +124,24 @@ export class ActivityStore {
       }
     };
 
+    @computed get activitiesByDate() {
+      return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    };
+
     getActivity = (id: string) => {
       return this.activityRegistry.get(id);
     }
 
-    @computed get activitiesByDate() {
-      return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
-    };
+    groupActivitiesByDate (activities: IActivity[]) {
+      const sortedActivities = activities.sort(
+        (a, b) => Date.parse(a.date) - Date.parse(b.date)
+      );
+      return Object.entries(sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split('T')[0];
+        activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        return activities;
+      }, {} as {[key: string]: IActivity[]}));
+    }
 }
 
 // If don't wanna use decorators
